706f7e8c514ded30a9f63e1c91685dcf39cb3ce9
ranitachi <fachran.nazarullah@gmail.com>
2017-12-18 07:11:05 +0700

    update

--- resources/views/includes-backend/appscripts.blade.php
+++ resources/views/includes-backend/appscripts.blade.php
@@ -1,3 +1,4 @@
 <script type="text/javascript" src="{{ asset('theme/js/app.js') }}"></script>
 <script type="text/javascript" src="{{ asset('theme/js/app_plugins.js') }}"></script>
  <script type="text/javascript" src="{{ asset('theme/js/app_demo.js')}}"></script>
+ <script type="text/javascript" src="{{ asset('theme/js/app_demo_dashboard.js')}}"></script>
